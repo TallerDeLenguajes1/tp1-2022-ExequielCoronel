c4abf30df29aa13211644443bbc6168986b9eb5d
CoronelExequiel <execoronel14f@gmail.com>
2022-08-27 16:52:21 -0300

    Punto1_Problema1

---
+++ Punto2/Problema1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System; 
+namespace TPN1_Punto2
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int num;
+            
+            try{
+                System.Console.WriteLine("Ingrese un numero entero, para calcular su cuadrado: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                System.Console.WriteLine($"El cuadrado de {num} es {num*num}");
+            }catch(FormatException){
+                System.Console.WriteLine("Error: se supero el tamaño que soporta el tipo de dato entero.");
+            }catch(OverflowException){
+                System.Console.WriteLine("Error: el numero ingresado supera el tamaño soportado por el tipo de dato entero.");
+            }catch(Exception e){
+                System.Console.WriteLine($"Error:{e.Message}");
+            } 
+        }
+    }
+    
+}
