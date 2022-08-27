e3cfc1f6079a576e59e55e38d1dbc9b724fb58c0
CoronelExequiel <execoronel14f@gmail.com>
2022-08-27 17:08:03 -0300

    Punto2-Problema2

--- Punto2/Problema2/Program.cs
+++ Punto2/Problema2/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TPN1_Punto2
+{
+    using System;
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int num1, num2;
+            try
+            {
+                System.Console.WriteLine("Ingrese el dividendo: ");
+                num1=Convert.ToInt32(Console.ReadLine()); 
+                System.Console.WriteLine("Ingrese el divisor: ");
+                num2=Convert.ToInt32(Console.ReadLine());
+                System.Console.WriteLine($"El cociente entero {num1}/{num2} es igual a {num1/num2}");
+            }
+            catch(DivideByZeroException)
+            {
+                System.Console.WriteLine("Error: No esta definida la division por 0");
+            }
+            catch(OverflowException)
+            {
+                System.Console.WriteLine("Error: Se supero el tamaño soportado por el tipo de dato entero");
+            }
+            catch(FormatException)
+            {
+                System.Console.WriteLine("Error: No se ingreso el tipo de dato esperado");
+            }
+            catch(Exception e)
+            {
+                System.Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+    }
+}
