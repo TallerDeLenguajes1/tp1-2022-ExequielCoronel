ec01f2bb52d106a99419b9eeb673d1007b33bcc3
CoronelExequiel <execoronel14f@gmail.com>
2022-08-27 17:23:32 -0300

    Punto2-Problema3

--- Punto2/Problema3/Program.cs
+++ Punto2/Problema3/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TPN1_Punto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Km, Litros;
+            try
+            {
+                System.Console.WriteLine("Ingrese los kilómetros conducidos: ");
+                Km = Convert.ToInt32(Console.ReadLine());
+                System.Console.WriteLine("Ingrese los litros consumidos: ");
+                Litros = Convert.ToInt32(Console.ReadLine());
+                System.Console.WriteLine($"Los kilómetros recoridos por litro son: {Km/Litros} Km/L");
+            } 
+            catch(FormatException)
+            {
+                System.Console.WriteLine("Error: Tipo de dato no soportado");
+            }
+            catch(DivideByZeroException)
+            {
+                System.Console.WriteLine("Error: Los litros consumidos no pueden ser 0");
+            }
+            catch(Exception e)
+            {
+                System.Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+    }
+}
